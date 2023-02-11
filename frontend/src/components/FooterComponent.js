f81a60b6fe2d37b5f11ad0a3c973cace6b5c70e5
Singgih Aji Sasongko <singgih.aji2010@gmail.com>
2023-02-11 20:30:30 +0700

    style footer component

--- frontend/src/components/FooterComponent.js
+++ frontend/src/components/FooterComponent.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 function FooterComponent() {
-  return <div>FooterComponent</div>;
+  return (
+    <footer>
+      <Container fluid>
+        <Row className="mt-5">
+          <Col className="bg-dark text-white text-center py-5">Copyright &copy; Best Online Shop</Col>
+        </Row>
+      </Container>
+    </footer>
+  )
 }
 
 export default FooterComponent;
