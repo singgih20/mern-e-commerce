267455e939ccfaa16afbc2904651dd8161229a4c
Singgih Aji Sasongko <singgih.aji2010@gmail.com>
2023-02-15 20:56:35 +0700

    floating blue circle to open chat component

--- frontend/src/components/user/UserChatComponent.js
+++ frontend/src/components/user/UserChatComponent.js
@@ -1,5 +1,14 @@
+import "../../chat.css"
 const UserChatComponent = () => {
-    return <p>This is a user chat component</p>
+    return (
+        <>
+        <input type="checkbox" id="check" />
+        <label className="chat-btn" htmlFor="check">
+            <i class="bi bi-chat-dots comment"></i>
+            <i class="bi bi-x-circle close"></i>
+        </label>
+        </>
+    )
 }
 
 export default UserChatComponent
