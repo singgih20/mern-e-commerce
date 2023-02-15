b00a94c2f41007289c03dc274607cfcf497a0062
Singgih Aji Sasongko <singgih.aji2010@gmail.com>
2023-02-15 20:40:44 +0700

    customize home page

--- frontend/src/components/ProductCarouselComponent.js
+++ frontend/src/components/ProductCarouselComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Carousel from 'react-bootstrap/Carousel';
+import { LinkContainer } from "react-router-bootstrap";
 
 function ProductCarouselComponent() {
   return (
@@ -10,10 +11,13 @@ function ProductCarouselComponent() {
           className="d-block w-100"
           src="/images/carousel/carousel-1.png"
           alt="First slide"
+          style={{height: "300px", objectFit: "cover"}}
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <LinkContainer style={{cursor: "pointer"}} to ="/product-detail">
+            <h3>Bestseller in Laptops Category</h3>
+          </LinkContainer>
+          <p>Dell Inspiron 15 3000 Laptop, 15.6 inch HD</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -21,11 +25,14 @@ function ProductCarouselComponent() {
           className="d-block w-100"
           src="/images/carousel/carousel-2.png"
           alt="Second slide"
+          style={{height: "300px", objectFit: "cover"}}
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <LinkContainer style={{cursor: "pointer"}} to ="/product-detail">
+            <h3>Bestseller in Book Category</h3>
+          </LinkContainer>
+          <p>World of Eric Carle, Hear Bear Roar 30-Button Animal Sound Book</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -33,13 +40,14 @@ function ProductCarouselComponent() {
           className="d-block w-100"
           src="/images/carousel/carousel-3.png"
           alt="Third slide"
+          style={{height: "300px", objectFit: "cover"}}
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <LinkContainer style={{cursor: "pointer"}} to ="/product-detail">
+            <h3>Bestseller in Camera Category</h3>
+          </LinkContainer>
+          <p>4k Camcorder Video Camera 60FPS 48MP Vlogging Camera for YouTube WiFi 16X Digital Camera</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,10 +1,31 @@
 import React from "react";
 import { ProductCarouselComponent, CategoryCardComponent } from '../components/'
+import { Row, Container } from "react-bootstrap";
 export default function HomePage() {
+  const categories = [
+    'Tablets',
+    'Monitors',
+    'Games',
+    'Printers',
+    'Software',
+    'Camera',
+    'Books',
+    'Videos'
+  ]
   return (
     <div>
       <ProductCarouselComponent />
-      <CategoryCardComponent />
+      <Container>
+        <Row xs={1} md={2} className="g-4 mt-5">
+          {
+            categories.map((category, idx) => {
+              return(
+                <CategoryCardComponent category={category} idx={idx} key={idx} />
+              )
+            })
+          }
+        </Row>
+      </Container>
     </div>
   );
 }
